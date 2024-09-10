8be075d78f1f38ef97d32d8b26eb962cc148408d
sasha <lujannatasha1989@gmail.com>
2024-09-10 19:12:39 -0300

    acomode error de traer datos del desarrollador

--- diarioDigital/templates/showDeveloper.tpl
+++ diarioDigital/templates/showDeveloper.tpl
@@ -3,9 +3,9 @@
 <div >
     <div >
         <div class="card card-body" >
-                <h5 class="mb-5">$nombre</h5>
-                <h5 class="mb-5">$email</h5>
-                <h5 class="mb-5">$rol</h5>
+                <h5 class="mb-5">{strtoupper($nombre)}</h5>
+                <h5 class="mb-5">{strtoupper($email)}</h5>
+                <h5 class="mb-5">{strtoupper($rol)}</h5>
         </div>
     </div>
 </div>
--- diarioDigital/templates_c/823d94e7e10b44a851fc6a685a55897178276512_0.file.showDeveloper.tpl.php
+++ diarioDigital/templates_c/823d94e7e10b44a851fc6a685a55897178276512_0.file.showDeveloper.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2024-09-07 02:06:22
+/* Smarty version 3.1.34-dev-7, created on 2024-09-11 00:09:33
   from 'C:\xampp\htdocs\web22024\diarioDigital\templates\showDeveloper.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_66db98fe3baf84_14664904',
+  'unifunc' => 'content_66e0c39d064e81_19361260',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '823d94e7e10b44a851fc6a685a55897178276512' => 
     array (
       0 => 'C:\\xampp\\htdocs\\web22024\\diarioDigital\\templates\\showDeveloper.tpl',
-      1 => 1725667175,
+      1 => 1726006162,
       2 => 'file',
     ),
   ),
@@ -22,16 +22,19 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:footer.tpl' => 1,
   ),
 ),false)) {
-function content_66db98fe3baf84_14664904 (Smarty_Internal_Template $_smarty_tpl) {
+function content_66e0c39d064e81_19361260 (Smarty_Internal_Template $_smarty_tpl) {
 ?> <?php $_smarty_tpl->_subTemplateRender('file:header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
 <div >
     <div >
         <div class="card card-body" >
-                <h5 class="mb-5">$nombre</h5>
-                <h5 class="mb-5">$email</h5>
-                <h5 class="mb-5">$rol</h5>
+                <h5 class="mb-5"><?php echo strtoupper($_smarty_tpl->tpl_vars['nombre']->value);?>
+</h5>
+                <h5 class="mb-5"><?php echo strtoupper($_smarty_tpl->tpl_vars['email']->value);?>
+</h5>
+                <h5 class="mb-5"><?php echo strtoupper($_smarty_tpl->tpl_vars['rol']->value);?>
+</h5>
         </div>
     </div>
 </div>
